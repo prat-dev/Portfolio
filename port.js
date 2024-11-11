df589d5dab1e84c4c2b1e383d0ce72830ff2c370
prat-dev <ranjanprateeklko2000@gmail.com>
2024-11-12 02:23:41 +0530

    adding functionality

---
+++ port.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () =>{
+const downloadButton = document.getElementById('download-resume');
+downloadButton.addEventListener('click', () => {
+    const link = document.createElement('a');
+    link.href = 'resume.pdf';
+    link.download = 'resume.pdf';
+    link.click();
+  
+})
+  
+  const githubbtn = document.getElementById('github-link');
+    const githubUrl = ("https://github.com/prat-dev")
+    githubbtn.addEventListener('click', () => {
+       window.open(githubUrl,'_blank');
+    })
+})
